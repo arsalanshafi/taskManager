4f979af0b95035534c9ac6d42ecc2763e5994475
abuArsalan <arsalanshafi911@gmail.com>
2024-02-06 13:11:04 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//task manager
+const addTaskBtn = document.querySelector(".addTaskBtn");
+const dialog = document.querySelector(".addTask");
+const add = document.querySelector(".add");
+
+
+addTaskBtn.addEventListener("click", () => {
+    dialog.classList.add("show");
+})
+
+add.addEventListener("click", () => {
+    dialog.classList.remove("show");
+})
