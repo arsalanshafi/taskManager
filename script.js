4e9925ff187b3e64d8c5012c3990ae04d48ef881
abuArsalan <arsalanshafi911@gmail.com>
2024-02-06 19:11:22 +0530

    commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const dialog = document.querySelector(".addTask");
 const add = document.querySelector(".add");
 const container = document.querySelector(".tasks");
 const cancelBtn = document.querySelector(".cancel")
+let str = "";
 
 
 window.onload = () => {
@@ -30,12 +31,14 @@ function addTask(){
     let taskTitle = document.querySelector('.title').value;
     let taskCategory = document.querySelector(".category").value;
 
-    let task = `${taskTitle},${taskCategory}`
-    let count = localStorage.getItem("taskCount");
-    count = Number(count) + 1;
-    localStorage.setItem("taskCount",count);
-    console.log(count);
-    localStorage.setItem(`task${count}`,task);
+    if(localStorage.getItem("value") === null || localStorage.getItem("value")=== ""){
+        str = str +`${taskTitle},${taskCategory}`
+    }else{
+        str = str + `,${taskTitle},${taskCategory}`
+    }
+
+
+    localStorage.setItem("value",str)
 }
 
 
@@ -43,27 +46,59 @@ function addTask(){
 function displayTasks(){
 
     container.innerHTML = "";
-    for(let i = 1;i <= Number(localStorage.taskCount);i++){
-
-        let data = localStorage.getItem(`task${i}`).split(",")
-        const task = document.createElement("div");
-        task.classList.add("task");
-        task.innerHTML = `<input type="checkbox" name="" id="" class="check">
-        <div class="taskInfo">
-            <div class="taskName">${data[0]}</div>
-            <div class="dd">12-10-2024</div>
-            <div class="icon">jj</div>
-        </div>`
-        container.appendChild(task);
+    if(localStorage.getItem("value") === null || localStorage.getItem("value")=== ""){
+        localStorage.clear();
+        return;
     }
 
+    let arr = localStorage.getItem("value").split(",");
+    // console.log(arr);
+    
+    for(let i = 0;i < arr.length;i+=2){
+        let ele = document.createElement("div");
+        ele.classList.add("task");
+        ele.innerHTML = `<input type="checkbox" name="" id="" class="check" onclick = checkTask(this)>
+        <div class="taskInfo">
+            <div class="taskName">${arr[i]}</div>
+            <div class="dd">12-10-2024</div>
+            <div class="icon" style="cursor:pointer;" data-num = ${i} onclick = deletetask(this)>&#10006</div>
+        </div>`
+        container.appendChild(ele);
+    }
+    
+    
+
 }
+
+
 
 function clearForm(){
     document.querySelector(".title").value = "";
     document.querySelector(".category").value = "";
 }
 
+
+function deletetask(target){
+    let num = target.getAttribute("data-num");
+    let arr = localStorage.getItem("value").split(",");
+    arr.splice(num,2);
+    str = arr.join();
+    localStorage.setItem("value",str);
+    displayTasks();
+    
+}
+
+function checkTask(target){
+    if(target.checked){
+
+        target.nextElementSibling.firstElementChild.style.textDecoration = "line-through";
+        return;
+    }
+    
+    target.nextElementSibling.firstElementChild.style.textDecoration = "none";
+    return;
+    
+}
 
 /* <div class="task">
         <input type="checkbox" name="" id="" class="check">
